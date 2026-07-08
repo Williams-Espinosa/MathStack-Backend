dd23c8e31b385cfae300283da0216c395f5403f3
Williams Espinosa López <w.espinosa.it@gmail.com>
2026-07-08 12:25:47 -0600

    Corrige importación faltante en AdminRouting

--- bin/main/com/mathstack/admin/infrastructure/rest/AdminRouting.kt
+++ bin/main/com/mathstack/admin/infrastructure/rest/AdminRouting.kt
@@ -15,6 +15,7 @@ import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
